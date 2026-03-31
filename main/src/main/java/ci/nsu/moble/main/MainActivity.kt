24e05f64ce65459334950de4c383a2195a910094
dallix <vladushka.potapov@gmail.com>
2026-04-01 04:48:20 +0700

    update mainActivity

--- main/src/main/java/ci/nsu/moble/main/MainActivity.kt
+++ main/src/main/java/ci/nsu/moble/main/MainActivity.kt
@@ -4,18 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import ci.nsu.moble.main.ui.theme.PracticeTheme
+import androidx.lifecycle.viewmodel.compose.viewModel
+
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,27 +41,32 @@ class MainActivity : ComponentActivity() {
     }
 }
 @Composable
-fun MainScreen(modifier: Modifier = Modifier) {
+fun MainScreen(modifier: Modifier = Modifier, viewModel: CounterViewModel = viewModel()) {
+
+    val uiState by viewModel.uiState.collectAsState()
+
     Column(
         modifier = modifier.fillMaxSize().padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Button(
-            onClick = {
-
-            },
-            modifier = Modifier.padding(top = 16.dp)
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(8.dp)
         ) {
-            Text("inc")
+            Button(onClick = { viewModel.increment() }) {
+                Text("+")
+            }
+            Button(onClick = { viewModel.reset() }) {
+                Text("reset")
+            }
+            Button(onClick = { viewModel.decrement() }) {
+                Text("-")
+            }
         }
 
-        Button(
-            onClick = {
+        Spacer(modifier = Modifier.height(16.dp))
 
-            },
-            modifier = Modifier.padding(top = 16.dp)
-        ) {
-            Text("dec")
+        LazyColumn {
+            items(uiState.history){lang -> Text(lang)}
         }
     }
 }
