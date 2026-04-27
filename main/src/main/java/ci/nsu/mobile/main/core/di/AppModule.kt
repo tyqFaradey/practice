2cbab3602e91563a509725c27aa28b7df9753c00
Dallix <vladushka.potapov@gmail.com>
2026-04-28 04:42:02 +0700

    implement di

--- main/src/main/java/ci/nsu/mobile/main/core/di/AppModule.kt
+++ main/src/main/java/ci/nsu/mobile/main/core/di/AppModule.kt
@@ -1,2 +1,9 @@
 package ci.nsu.mobile.main.core.di
 
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {}
