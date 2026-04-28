fc2457ae1d6aac701a59905029bc97abc279347b
Dallix <vladushka.potapov@gmail.com>
2026-04-28 21:58:20 +0700

    add token manager

---
+++ main/src/main/java/ci/nsu/mobile/main/data/local/TokenManager.kt
@@ -0,0 +1,25 @@
+package ci.nsu.mobile.main.data.local
+
+import android.content.Context
+import androidx.core.content.edit
+
+class TokenManager(context: Context) {
+
+    private val prefs = context.getSharedPreferences("auth_prefs", Context.MODE_PRIVATE)
+
+    companion object {
+        private const val KEY_TOKEN = "jwt_token"
+    }
+
+    fun saveToken(token: String) {
+        prefs.edit { putString(KEY_TOKEN, token) }
+    }
+
+    fun getToken(): String? {
+        return prefs.getString(KEY_TOKEN, null)
+    }
+
+    fun clearToken() {
+        prefs.edit { remove(KEY_TOKEN) }
+    }
+}
